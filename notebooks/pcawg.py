0f9ab38b55a6b348fc322830605c09f23f3f9bfc
tkau93@gmail.com <tkau93@gmail.com>
2021-05-04 16:22:03 +0200

    Updated pcawg.py to new workflow

--- notebooks/pcawg.py
+++ notebooks/pcawg.py
@@ -36,14 +36,15 @@ LOAD = False
 #%% FSTs
 T_wgd_asymm = fstlib.Fst.read('../objects/wgd_asymm.fst')
 T_no_wgd_asymm = fstlib.Fst.read('../objects/no_wgd_asymm.fst')
+symbol_table = T_wgd_asymm.input_symbols()
 
 #%% folders and HDF5 store
-pcawg_folder = '/mnt/d/data/Documents/Projects/project-pcawg'
+pcawg_folder = '../../data/PCAWG'
 data_folder = os.path.join(pcawg_folder, 'consensus.20170119.somatic.cna.annotated')
 metadata_folder = os.path.join(pcawg_folder, 'metadata')
 
 #%% open HDF5 store
-hdfstore = pd.HDFStore(os.path.join(pcawg_folder, 'pcawg_scna.hdf5'))
+hdfstore = pd.HDFStore(os.path.join(pcawg_folder, 'pcawg_scna_.hdf5'))
 
 #%% metadata
 if LOAD:
@@ -169,7 +170,7 @@ else:
 
     ## phase CNPs
     diploid = fstlib.Fst()
-    diploid.set_input_symbols(T_wgd_asymm.input_symbols())
+    diploid.set_input_symbols(symbol_table)
     diploid.set_output_symbols(T_wgd_asymm.output_symbols())
     diploid.add_state()
     diploid.set_start(0)
@@ -182,9 +183,9 @@ else:
     phasing_scores = {}
     phased_dfs = []
     for idx, df in dat.groupby('sample_id'):
-        phasing_dict = medicc.create_phasing_fsa_dict_from_df(df, T_wgd_asymm.input_symbols(), 'X')
+        phasing_dict = medicc.create_phasing_fsa_dict_from_df(df, symbol_table, 'X')
         fsa_dict_a, fsa_dict_b, scores = medicc.phase_dict(phasing_dict, T_wgd_asymm, diploid)
-        phased = medicc.create_df_from_fsa_dicts(df, [fsa_dict_a, fsa_dict_b], 'X')
+        phased = medicc.create_df_from_fsa(df, [fsa_dict_a, fsa_dict_b], 'X')
         phased.columns = ['cn_a', 'cn_b']
         phased_dfs.append(phased)
         phasing_scores.update(scores)
@@ -201,31 +202,73 @@ else:
 if LOAD:
     distances = hdfstore['distances']
 else:
-    fsa_dict_a = medicc.create_standard_fsa_dict_from_allele_column(dat['cn_a'], T_wgd_asymm.input_symbols(), 'X')
-    fsa_dict_b = medicc.create_standard_fsa_dict_from_allele_column(dat['cn_b'], T_wgd_asymm.input_symbols(), 'X')
-    dist_no_wgd_a = pd.Series({aliquot_id:float(fstlib.score(T_no_wgd_asymm, diploid, fsa)) for aliquot_id, fsa in fsa_dict_a.items()}, name='dist_no_wgd_a')
-    dist_no_wgd_b = pd.Series({aliquot_id:float(fstlib.score(T_no_wgd_asymm, diploid, fsa)) for aliquot_id, fsa in fsa_dict_b.items()}, name='dist_no_wgd_b')
-    dist_no_wgd_total = dist_no_wgd_a + dist_no_wgd_b
-    dist_no_wgd_total.name='dist_no_wgd_total'
+    # Calc distance without WGD
+    fsa_dict = medicc.create_standard_fsa_dict_from_data(dat[['cn_a', 'cn_b']], symbol_table, 'X')
+    dist_no_wgd = pd.Series({aliquot_id:float(fstlib.score(T_no_wgd_asymm, diploid, fsa)) 
+                             for aliquot_id, fsa in fsa_dict.items()}, name='dist_no_wgd_a')
+    dist_no_wgd.name = 'dist_no_wgd'
 
-    ## determine wgd distances (are in sum equivalent to the phasing scores)
-    dist_wgd_a = pd.Series({aliquot_id:float(fstlib.score(T_wgd_asymm, diploid, fsa)) for aliquot_id, fsa in fsa_dict_a.items()}, name='dist_wgd_a')
-    dist_wgd_b = pd.Series({aliquot_id:float(fstlib.score(T_wgd_asymm, diploid, fsa)) for aliquot_id, fsa in fsa_dict_b.items()}, name='dist_wgd_b')
-    dist_wgd_total = dist_wgd_a + dist_wgd_b
-    dist_wgd_total.name='dist_wgd_total'
+    # Calc distance with WGD
+    dist_wgd = pd.Series({aliquot_id: float(fstlib.score(T_wgd_asymm, diploid, fsa))
+                             for aliquot_id, fsa in fsa_dict.items()}, name='dist_wgd_a')
+    dist_wgd.name = 'dist_wgd'
 
-    distances = pd.concat([dist_no_wgd_a, dist_no_wgd_b, dist_no_wgd_total, dist_wgd_a, dist_wgd_b, dist_wgd_total], axis=1)
-    distances['dist_a_diff'] = distances['dist_no_wgd_a'] - distances['dist_wgd_a']
-    distances['dist_b_diff'] = distances['dist_no_wgd_b'] - distances['dist_wgd_b']
-    distances['dist_total_diff'] = distances['dist_no_wgd_total'] - distances['dist_wgd_total']
+    distances = pd.concat([dist_no_wgd, dist_wgd], axis=1)
+    distances['dist_diff'] = distances['dist_no_wgd'] - distances['dist_wgd']
 
     hdfstore.put('distances', distances, format='table')
 
+#%% Bootstrap for WGD detection
+# This cell will take some time to finish
+if LOAD:
+    bootstrap_distances = hdfstore['bootstrap_distances']
+else:
+    N_bootstrap = 50
+    samples = np.unique(dat.index.get_level_values('sample_id'))
+
+    diploid_fsa = fstlib.Fst()
+    diploid_fsa.set_input_symbols(symbol_table)
+    diploid_fsa.set_output_symbols(T_wgd_asymm.output_symbols())
+    diploid_fsa.add_state()
+    diploid_fsa.set_start(0)
+    diploid_fsa.set_final(0, 0)
+    diploid_fsa.add_arc(0, ('1', '1', 0, 0))
+    diploid_fsa.add_arc(0, ('X', 'X', 0, 0))
+
+    bootstrap_distances = -1*np.ones((len(samples), 2, N_bootstrap))
+
+    for i in np.arange(N_bootstrap):
+        bootstrap_dat = medicc.bootstrap.chr_wise_bootstrap_df(dat)
+
+        FSA_dict = medicc.create_standard_fsa_dict_from_data(
+            bootstrap_dat, symbol_table, 'X')
+        dist_no_wgd = pd.Series({aliquot_id: float(fstlib.score(T_no_wgd_asymm, diploid_fsa, fsa))
+                                 for aliquot_id, fsa in FSA_dict.items()}, name='dist_no_wgd')
+        dist_wgd = pd.Series({aliquot_id: float(fstlib.score(T_wgd_asymm, diploid_fsa, fsa))
+                              for aliquot_id, fsa in FSA_dict.items()}, name='dist_wgd')
+
+        bootstrap_distances[:, 0, i] = dist_wgd
+        bootstrap_distances[:, 1, i] = dist_no_wgd
+
+    bootstrap_distances = pd.dataFrame(index=samples, columns=['wgd_status', 'mean_wgd', 'mean_no_wgd', 'diff_mean',
+                                                               'diff_std', 'diff_frac_zero'])
+
+    bootstrap_distances['pcawg_wgd'] = meta.loc[samples, 'pcawg_wgd']
+
+    bootstrap_distances['mean_wgd'] = np.mean(bootstrap_distances[:, 0, :], axis=1)
+    bootstrap_distances['mean_no_wgd'] = np.mean(bootstrap_distances[:, 1, :], axis=1)
+
+    bootstrap_distances['diff_mean'] = np.mean(
+        bootstrap_distances[:, 1, :] - bootstrap_distances[:, 0, :], axis=1)
+
+    hdfstore.put('bootstrap_distances', bootstrap_distances, format='table')
 # %%
 hdfstore.close()
+
+#%%
 result = meta.join(distances, how='inner').join(wgd[['ploidy_pcawg','hom']])
 result['wgd_status'] = result['wgd_status'].map({'wgd':'WGD', 'no_wgd':'No WGD'})
-result['wgd_status_medicc'] = (result['dist_total_diff']>=4).map({True:'WGD', False:'No WGD'})
+result['wgd_status_medicc'] = (result['dist_diff'] >= 1).map({True: 'WGD', False: 'No WGD'})
 linex = np.linspace(0, result.hom.max())
 linea = -2
 lineb = -1
@@ -237,16 +280,26 @@ linedist = -(linea * result.hom + lineb * result.ploidy_pcawg + linec) / np.sqrt
 result['linedist'] = linedist
 scale = 0.8
 
-#%% test for association with uncertainty
-confusion = pd.crosstab(result['wgd_status'], result['wgd_status_medicc'])
-sp.stats.chi2_contingency(pd.crosstab(result.eval("wgd_status != wgd_status_medicc"), result['wgd_uncertain']))
+#%% Overlap with PCAWG labels
+confusion_matrix = pd.crosstab(result['wgd_status'], result['wgd_status_medicc'])
 
+print(confusion_matrix)
+confusion_matrix = confusion_matrix.astype(int)
+print('{:.1f}% correct classification'.format(
+    100*(confusion_matrix.iloc[0, 0] + confusion_matrix.iloc[1, 1]) / confusion_matrix.sum().sum()))
+
+#%% Overlap with PCAWG labels from Bootstrap
+confusion_matrix_bootstrap = pd.crosstab(
+    bootstrap_distances['pcawg_wgd'], bootstrap_distances['diff_mean'] != 0.0)
+print(confusion_matrix_bootstrap)
+confusion_matrix_bootstrap = confusion_matrix_bootstrap.astype(int)
+print('{:.1f}% correct classification'.format(
+    100*(confusion_matrix_bootstrap.iloc[0, 0] + confusion_matrix_bootstrap.iloc[1, 1]) / confusion_matrix_bootstrap.sum().sum()))
 # %% plot PCAWG MED distributions
 palette = dict(zip(tumour_types.index, tumour_types.Hexadecimal))
 fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,10))
 plotdat = result.query("histology_useable").sort_values('histology_abbreviation')
-sns.boxplot(x='dist_wgd_total', y='histology_abbreviation', palette=palette, data=plotdat, ax=ax)
-#sns.violinplot(x='dist_wgd_total', y='histology_abbreviation', palette=palette, data=plotdat, ax=ax, scale='width')
+sns.boxplot(x='dist_wgd', y='histology_abbreviation', palette=palette, data=plotdat, ax=ax)
 ax.set_xlabel('MED (WGD) from diploid normal')
 ax.set_ylabel('PCAWG histology')
 fig.show()
@@ -254,8 +307,7 @@ fig.savefig('figures/pcawg_supp_MED_per_cancer_type.pdf', bbox_inches='tight')
 
 fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,10))
 plotdat = result.query("histology_useable").sort_values('histology_abbreviation')
-sns.boxplot(x='dist_total_diff', y='histology_abbreviation', palette=palette, data=plotdat, ax=ax)
-#sns.violinplot(x='dist_wgd_total', y='histology_abbreviation', palette=palette, data=plotdat, ax=ax, scale='width')
+sns.boxplot(x='dist_diff', y='histology_abbreviation', palette=palette, data=plotdat, ax=ax)
 ax.set_xlabel('MEDICC2 WGD score')
 ax.set_ylabel('PCAWG histology')
 fig.show()
@@ -292,18 +344,15 @@ axhist = fig.add_subplot(gs[0])
 #axhist.spines["left"].set_visible(False)
 axhist.spines["top"].set_visible(False)
 axhist.spines["right"].set_visible(False)
-sns.histplot(x='dist_total_diff', hue='wgd_status', legend=True, data=plotdat, ax=axhist)
+sns.histplot(x='dist_diff', hue='wgd_status', legend=True, data=plotdat, ax=axhist)
 axhist.get_legend().set_title('WGD status')
 
 ax = fig.add_subplot(gs[1])
 ax.spines['top'].set_visible(False)
-sns.scatterplot(x='dist_total_diff', y='linedist', data=plotdat, hue='wgd_status', ax=ax, legend=False)
+sns.scatterplot(x='dist_diff', y='linedist', data=plotdat, hue='wgd_status', ax=ax, legend=False)
 ax.set_xlabel('MEDICC2 WGD score')
 ax.set_ylabel('PCAWG WGD score')
-r = sp.stats.pearsonr(plotdat['dist_total_diff'], plotdat['linedist'])[0]**2
+r = sp.stats.pearsonr(plotdat['dist_diff'], plotdat['linedist'])[0]**2
 ax.text(0.98, 0.02, r"$r^2 = %.2f$" % r, transform=ax.transAxes, ha='right', va='bottom')
 #fig.show()
 fig.savefig('figures/pcawg_our_score_vs_linedist.pdf', bbox_inches='tight')
-
-
-# %%
