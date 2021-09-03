208944cd44cd7e734c571cc2a46a1a91ddd76695
tkau93@gmail.com <tkau93@gmail.com>
2021-09-03 10:11:13 +0200

    Updated to Fig2D and Supp 3

--- Figures_Kaufmann_et_al_2021/Fig_2D_and_Supp_3A_and_Supp_3B.py
+++ Figures_Kaufmann_et_al_2021/Fig_2D_and_Supp_3A_and_Supp_3B.py
@@ -15,10 +15,8 @@ linex = np.linspace(0, plotting_data['hom'].max())
 linea = -2
 linec = 2.9
 liney = linea * linex + linec
-# %% Figure with false MEDICC predictions
-# plotting_data[['hom', 'ploidy_pcawg', 'pcawg_wgd']].to_csv('../Figures_Kaufmann_et_al_2021/data/Fig_2D.tsv',
-#                                                     sep='\t', index=False)
 
+#%% Fig 2D: MEDICC predictions
 fig, ax = plt.subplots(figsize=(plotting_params['WIDTH_HALF'], plotting_params['WIDTH_HALF']))
 sns.scatterplot(x='hom', y='ploidy_pcawg', data=plotting_data, hue='pcawg_wgd', ax=ax, hue_order=['No WGD', 'WGD'])
 sns.scatterplot(x='hom', y='ploidy_pcawg', data=plotting_data.loc[plotting_data['bootstrap_2_wgds']],
@@ -36,25 +34,34 @@ fig.savefig('../Figures_Kaufmann_et_al_2021/final_figures/Fig_2D.pdf', bbox_inch
 fig.savefig('../Figures_Kaufmann_et_al_2021/final_figures/Fig_2D.png', bbox_inches='tight', dpi=600)
 
 
-# %% plot PCAWG MED distributions
+# #%% Supp 3A: MEDICC MED distributions
 palette = dict(zip(pcawg_color_palette.index, pcawg_color_palette['Hexadecimal']))
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(
-    plotting_params['WIDTH_HALF'], plotting_params['WIDTH_FULL']))
-cur_plotting_data = plotting_data.query("histology_useable").sort_values('histology_abbreviation')
-sns.boxplot(x='dist_wgd', y='histology_abbreviation', palette=palette, data=cur_plotting_data, ax=ax)
-ax.set_xlabel('MED (WGD) from diploid normal')
-ax.set_ylabel('PCAWG histology')
-fig.savefig('../Figures_Kaufmann_et_al_2021/final_figures/Supp_3A.pdf', bbox_inches='tight')
-fig.savefig('../Figures_Kaufmann_et_al_2021/final_figures/Supp_3A.png',
-            bbox_inches='tight', dpi=600)
+fig, axs = plt.subplots(nrows=1, ncols=3, sharey=True,
+                        figsize=(3*plotting_params['WIDTH_HALF'], plotting_params['WIDTH_FULL']))
 
-#%%
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(
-    plotting_params['WIDTH_HALF'], plotting_params['WIDTH_FULL']))
 cur_plotting_data = plotting_data.query("histology_useable").sort_values('histology_abbreviation')
-sns.boxplot(x='dist_diff', y='histology_abbreviation', palette=palette, data=cur_plotting_data, ax=ax)
-ax.set_xlabel('MEDICC2 WGD score')
-ax.set_ylabel('PCAWG histology')
-fig.savefig('../Figures_Kaufmann_et_al_2021/final_figures/Supp_3B.pdf', bbox_inches='tight')
-fig.savefig('../Figures_Kaufmann_et_al_2021/final_figures/Supp_3B.png',
+sns.boxplot(x='dist_wgd', y='histology_abbreviation', palette=palette, data=cur_plotting_data, ax=axs[0])
+axs[0].set_xlabel('MED (WGD) from diploid normal')
+axs[0].set_ylabel('PCAWG histology')
+
+#%% Supp 3B: MEDICC2 WGD score distributions
+cur_plotting_data = plotting_data.query("histology_useable").sort_values('histology_abbreviation')
+sns.boxplot(x='dist_diff', y='histology_abbreviation', palette=palette, data=cur_plotting_data, ax=axs[1])
+axs[1].set_xlabel('MEDICC2 WGD score')
+axs[1].set_ylabel('')
+# axs[1].set_yticklabels([])
+
+#%% Supp 3C: MEDICC2 WGD score distributions
+plotting_data['has_wgd'] = plotting_data['wgd_status_medicc_bootstrap'] == 'WGD'
+cur_plotting_data = plotting_data.query("histology_useable").groupby(
+    'histology_abbreviation').mean()[['has_wgd']]
+
+sns.barplot(x='has_wgd', y=cur_plotting_data.index, orient='h', data=cur_plotting_data, 
+            palette=palette, ax=axs[2], edgecolor='black', linewidth=1)
+axs[2].set_xlabel('Fraction of samples exhibiting WGD')
+axs[2].set_ylabel('')
+# axs[2].set_yticklabels([])
+
+fig.savefig('../Figures_Kaufmann_et_al_2021/final_figures/Supp_3A_3B_3C.pdf', bbox_inches='tight')
+fig.savefig('../Figures_Kaufmann_et_al_2021/final_figures/Supp_3A_3B_3C.png',
             bbox_inches='tight', dpi=600)
