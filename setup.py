77055d888d50778cdc1b12f49ff53b17e32d084e
tkau93@gmail.com <tkau93@gmail.com>
2021-09-15 15:29:52 +0200

    Created files necessary for build

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 import sys
+from pathlib import Path
 
+import numpy as np
 from Cython.Build import cythonize
 from setuptools import Extension, setup
 
 sys.path.append('fstlib/cext')
 
+this_directory = Path(__file__).parent
+print(this_directory)
+print(this_directory.absolute())
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='MEDICC2',
     version='0.5b1',
-    author='Tom Kaufmann, Roland F Schwarz, Marina Petkovic',
+    author='Tom L Kaufmann, Roland F Schwarz, Marina Petkovic',
     author_email='tkau93@gmail.com, roland.f.schwarz@gmail.com, marina.55kovic@gmail.com',
     description='Minimum Event Distance for Intra-tumour Copy-number Comparisons',
-    long_description=open('README.MD').read(),    
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://bitbucket.org/schwarzlab/medicc2',
     classifiers=[
                 "Programming Language :: Python :: 3",
@@ -24,6 +32,13 @@ setup(
              'medicc2.py',
              'tools/medicc-phase.py'],
     license='GPL-3',
+    include_dirs=np.get_include(),
+    install_requires=[
+        'numpy>=1.20.1'
+    ],
+    package_data={
+        "": ["objects/*"],
+    },
     ext_modules = cythonize([
         Extension("fstlib.cext.pywrapfst", 
                   ["fstlib/cext/pywrapfst.pyx"],
