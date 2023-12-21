a3d9c9a4e434a69a13c8b4562b20616a46b95300
Tom Kaufmann <tkau93@gmail.com>
2023-12-21 01:04:10 +0100

    added legacy_loh_asymm_fst

--- tools/create_all_fsts.sh
+++ tools/create_all_fsts.sh
@@ -8,3 +8,4 @@ python ../tools/medicc-create-cn-fst.py ../medicc/objects --max-cn 8 --sep X --w
 python ../tools/medicc-create-cn-fst.py ../medicc/objects --max-cn 8 --sep X --prefix no_wgd
 python ../tools/medicc-create-cn-fst.py ../medicc/objects --max-cn 8 --sep X --wgd --max-num-wgds 1 --prefix wgd_1
 python ../tools/medicc-create-cn-fst.py ../medicc/objects --max-cn 8 --sep X --wgd --max-num-wgds 2 --prefix wgd_2
+python ../tools/medicc-create-cn-fst.py ../medicc/objects --max-cn 8 --sep X --wgd --prefix legacy_loh --legacy-loh
